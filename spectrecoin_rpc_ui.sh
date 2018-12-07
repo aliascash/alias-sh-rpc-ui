3d880321d6cacb2cdf1994f98eb703b81697840b
HLXEasy <hlxeasy@gmail.com>
2018-12-07 20:01:11 +0100

    Implemented determination of all wallet info entries

--- spectrecoin_rpc_ui.sh
+++ spectrecoin_rpc_ui.sh
@@ -291,12 +291,19 @@ getInfo() {
     local _oldIFS=$IFS
     local _itemBuffer
     local _unixtime
+
+    # Remove leading '{' and trailing '}'
     curl_result_global=${curl_result_global#'{'}
     curl_result_global=${curl_result_global%'}'}
-    IFS=','
-    # if wallet is not encrypted
+
+    # Replace '{' and '}' inside of result string with ','
+    curl_result_global=${curl_result_global//\{/,}
+    curl_result_global=${curl_result_global//\}/,}
+
+    # Set default if wallet is not encrypted
     info_global[${WALLET_UNLOCKED_UNTIL}]="${TEXT_WALLET_HAS_NO_PW}"
-    curl_result_global=${curl_result_global%\}*}
+
+    IFS=','
     for _itemBuffer in ${curl_result_global}; do
         case ${_itemBuffer%%:*} in
             'version')
@@ -328,39 +335,37 @@ getInfo() {
                 else
                     info_global[${WALLET_ERRORS}]="\Z1${_itemBuffer#*:}\Zn"
                 fi;;
+            'mode')
+                info_global[${WALLET_MODE}]="${_itemBuffer#*:}";;
+            'state')
+                info_global[${WALLET_STATE}]="${_itemBuffer#*:}";;
+            'protocolversion')
+                info_global[${WALLET_PROTOCOLVERSION}]="${_itemBuffer#*:}";;
+            'walletversion')
+                info_global[${WALLET_WALLETVERSION}]="${_itemBuffer#*:}";;
+            'newmint')
+                info_global[${WALLET_NEWMINT}]="${_itemBuffer#*:}";;
+            'reserve')
+                info_global[${WALLET_RESERVE}]="${_itemBuffer#*:}";;
+            'blocks')
+                info_global[${WALLET_BLOCKS}]="${_itemBuffer#*:}";;
+            'timeoffset')
+                info_global[${WALLET_TIMEOFFSET}]="${_itemBuffer#*:}";;
+            'moneysupply')
+                info_global[${WALLET_MONEYSUPPLY}]="${_itemBuffer#*:}";;
+            'spectresupply')
+                info_global[${WALLET_SPECTRESUPPLY}]="${_itemBuffer#*:}";;
+            'proxy')
+                info_global[${WALLET_PROXY}]="${_itemBuffer#*:}";;
+            'proof-of-work')
+                # PoW is a sub-entry of 'difficulty'
+                info_global[${WALLET_PROOF_OF_WORK}]="${_itemBuffer#*:}";;
+            'proof-of-stake')
+                # PoS is a sub-entry of 'difficulty'
+                info_global[${WALLET_PROOF_OF_STAKE}]="${_itemBuffer#*:}";;
+            *)
+                ;;
         esac
-
-#        if [[ ${_itemBuffer} == 'version'* ]]; then
-#            info_global[0]="${_itemBuffer#*':'}"
-#        elif [[ ${_itemBuffer} == 'balance'* ]]; then
-#            info_global[1]="${_itemBuffer#*':'}"
-#        elif [[ ${_itemBuffer} == 'spectrebalance'* ]]; then
-#            info_global[2]="${_itemBuffer#*':'}"
-#        elif [[ ${_itemBuffer} == 'stake'* ]]; then
-#            info_global[3]="${_itemBuffer#*':'}"
-#        elif [[ ${_itemBuffer} == 'connections'* ]]; then
-#            info_global[4]="${_itemBuffer#*':'}"
-#        elif [[ ${_itemBuffer} == 'datareceived'* ]]; then
-#            info_global[5]="${_itemBuffer#*':'}"
-#        elif [[ ${_itemBuffer} == 'datasent'* ]]; then
-#            info_global[6]="${_itemBuffer#*':'}"
-#        elif [[ ${_itemBuffer} == 'ip'* ]]; then
-#            info_global[7]="${_itemBuffer#*':'}"
-#        elif [[ ${_itemBuffer} == 'unlocked_until:'* ]]; then
-#            _unixtime="${_itemBuffer#*':'}"
-#            if [[ "$_unixtime" -gt 0 ]]; then
-#                info_global[8]="${TEXT_WALLET_IS_UNLOCKED}"
-#            else
-#                info_global[8]="${TEXT_WALLET_IS_LOCKED}"
-#            fi
-#        elif [[ ${_itemBuffer} == 'errors'* ]]; then
-#            if [[ "${_itemBuffer#*':'}" == 'none' ]]; then
-#                info_global[9]="${TEXT_DAEMON_NO_ERRORS_DURING_RUNTIME}"
-#            else
-#                #todo remove } at the end of error msg
-#                info_global[9]="\Z1""${_itemBuffer#*':'}""\Zn"
-#            fi
-#        fi
     done
     IFS=${_oldIFS}
 }
