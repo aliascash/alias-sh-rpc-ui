2591c8164807cbe640ad0f78856b0638a95c49b3
HLXEasy <hlxeasy@gmail.com>
2018-12-07 15:23:05 +0100

    Defined constants on separate include file

---
+++ include/constants.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# ============================================================================
+# Definition of used constants
+#
+# Creation: 2018-12-07 HLXEasy
+# ============================================================================
+
+# Dialog exit status codes
+DIALOG_OK=0
+DIALOG_CANCEL=1
+DIALOG_HELP=2
+DIALOG_EXTRA=3
+DIALOG_ITEM_HELP=4
+DIALOG_ESC=255
+DIALOG_ERROR=-1
+
+
+# Fields on transaction array
+TA_ACCOUNT=0
+TA_ADDRESS=1
+TA_AMOUNT=2
+TA_BLOCKHASH=3
+TA_BLOCKINDEX=4
+TA_BLOCKTIME=5
+TA_CATEGORY=6
+TA_CONFIRMATIONS=7
+TA_CURRENCY=8
+TA_GENERATED=9
+TA_NARRATION=10
+TA_TIME=11
+TA_TIMERECEIVED=12
+TA_TXID=13
+TA_VERSION=14
+
+WALLET_VERSION=0
+WALLET_BALANCE_XSPEC=1
+WALLET_BALANCE_SPECTRE=2
+WALLET_STAKE=3
+WALLET_CONNECTIONS=4
+WALLET_DATARECEIVED=5
+WALLET_DATASENT=6
+WALLET_IP=7
+WALLET_UNLOCKED_UNTIL=8
+WALLET_ERRORS=9
