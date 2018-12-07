6ac96dcd343234137121461bff24f00378c4e7c9
HLXEasy <hlxeasy@gmail.com>
2018-12-07 21:45:46 +0100

    Implemented dialog to show wallet details

--- include/viewWalletInfo.sh
+++ include/viewWalletInfo.sh
@@ -12,53 +12,49 @@ makeWalletInfoOutput() {
     else
         _textWidth="$1"
     fi
-    if [[ ${TEXTHIGHT_INFO} -ge 13 ]] ; then
-        echo "${TEXT_HEADLINE_WALLET_INFO}\n"
-    fi
-    local _balance=$(echo "scale=8 ; ${info_global[${WALLET_BALANCE_XSPEC}]}+${info_global[${WALLET_STAKE}]}" | bc)
-    if [[ ${_balance} == '.'* ]]; then
-        _balance="0"${_balance}
-    fi
-    echo $(fillLine "${TEXT_BALANCE} ${TEXT_CURRENCY}:-_-${_balance}" \
-                    "${_textWidth}")"\n"
-    echo $(fillLine "${TEXT_BALANCE} ${TEXT_CURRENCY_2}:-_-\Z6${info_global[${WALLET_BALANCE_SPECTRE}]}\Zn" \
-                    "${_textWidth}")"\n"
-    #
-    if [[ ${TEXTHIGHT_INFO} -ge 13 ]] ; then
-        echo "\n${TEXT_HEADLINE_STAKING_INFO}\n"
-    elif [[ ${TEXTHIGHT_INFO} -ge 10 ]] ; then
-        echo "\n"
-    fi
-    echo $(fillLine "${TEXT_WALLET_STATE}: ${info_global[${WALLET_UNLOCKED_UNTIL}]}-_-${TEXT_STAKING_STATE}: ${stakinginfo_global[0]}" \
-                    "${_textWidth}")"\n"
-    echo $(fillLine "${TEXT_STAKING_COINS}: \Z4${info_global[${WALLET_BALANCE_XSPEC}]}\Zn-_-(\Z5${info_global[${WALLET_STAKE}]}\Zn ${TEXT_MATRUING_COINS})" \
-                    "${_textWidth}")"\n"
-    echo $(fillLine "${TEXT_EXP_TIME}: ${stakinginfo_global[1]}" \
-                    "${_textWidth}")"\n"
-    #
-    if [[ ${TEXTHIGHT_INFO} -ge 13 ]] ; then
-        echo "\n${TEXT_HEADLINE_CLIENT_INFO}\n"
-    elif [[ ${TEXTHIGHT_INFO} -ge 10 ]] ; then
-        echo "\n"
-    fi
-    echo $(fillLine "${TEXT_DAEMON_VERSION}: ${info_global[${WALLET_VERSION}]}-_-${TEXT_DAEMON_ERRORS_DURING_RUNTIME}: ${info_global[${WALLET_ERRORS}]}" \
-                    "${_textWidth}")"\n"
-    echo $(fillLine "${TEXT_DAEMON_IP}: ${info_global[${WALLET_IP}]}-_-${TEXT_DAEMON_PEERS}: ${info_global[${WALLET_CONNECTIONS}]}" \
-                    "${_textWidth}")"\n"
-    echo $(fillLine "${TEXT_DAEMON_DOWNLOADED_DATA}: ${info_global[${WALLET_DATARECEIVED}]}-_-${TEXT_DAEMON_UPLOADED_DATA}: ${info_global[${WALLET_DATASENT}]}" \
-                    "${_textWidth}")"\n"
+    echo "\n"
+    echo "Version:            ${info_global[${WALLET_VERSION}]}\n"
+    echo "Balance XSPEC:      ${info_global[${WALLET_BALANCE_XSPEC}]}\n"
+    echo "Balance SPECTRE:    ${info_global[${WALLET_BALANCE_SPECTRE}]}\n"
+    echo "Stake:              ${info_global[${WALLET_STAKE}]}\n"
+    echo "Connections:        ${info_global[${WALLET_CONNECTIONS}]}\n"
+    echo "Data received:      ${info_global[${WALLET_DATARECEIVED}]}\n"
+    echo "Data sent:          ${info_global[${WALLET_DATASENT}]}\n"
+    echo "IP:                 ${info_global[${WALLET_IP}]}\n"
+    echo "Unlocked until:     ${info_global[${WALLET_UNLOCKED_UNTIL}]}\n"
+    echo "Errors:             ${info_global[${WALLET_ERRORS}]}\n"
+    echo "Mode:               ${info_global[${WALLET_MODE}]}\n"
+    echo "State:              ${info_global[${WALLET_STATE}]}\n"
+    echo "Protocol version:   ${info_global[${WALLET_PROTOCOLVERSION}]}\n"
+    echo "Wallet version:     ${info_global[${WALLET_WALLETVERSION}]}\n"
+    echo "Newmint:            ${info_global[${WALLET_NEWMINT}]}\n"
+    echo "Reserve:            ${info_global[${WALLET_RESERVE}]}\n"
+    echo "Blocks:             ${info_global[${WALLET_BLOCKS}]}\n"
+    echo "Timeoffset:         ${info_global[${WALLET_TIMEOFFSET}]}\n"
+    echo "Moneysupply:        ${info_global[${WALLET_MONEYSUPPLY}]}\n"
+    echo "Spectresupply:      ${info_global[${WALLET_SPECTRESUPPLY}]}\n"
+    echo "Proxy:              ${info_global[${WALLET_PROXY}]}\n"
+    echo "Proof of work:      ${info_global[${WALLET_PROOF_OF_WORK}]}\n"
+    echo "Proof of stake:     ${info_global[${WALLET_PROOF_OF_STAKE}]}\n"
+    echo "Testnet:            ${info_global[${WALLET_TESTNET}]}\n"
+    echo "Keypoolsize:        ${info_global[${WALLET_KEYPOOLSIZE}]}\n"
+    echo "Paytxfee:           ${info_global[${WALLET_PAYTXFEE}]}\n"
+    echo "Mininput:           ${info_global[${WALLET_MININPUT}]}\n"
 }
 
 viewWalletInfo() {
     _mainMenuButton="${BUTTON_LABEL_MAIN_MENU}"
     calculateLayout
+    executeCURL "getinfo"
+    drawGauge "48" \
+            "${TEXT_GAUGE_PROCESS_INFO}"
     getInfo
     dialog --backtitle "${TITLE_BACK}" \
            --colors \
-           --title "${TITLE_ERROR}" \
+           --title "${TITLE_WALLET_INFO}" \
            --ok-label "${BUTTON_LABEL_OK}" \
            --no-shadow \
-           --msgbox "$(makeWalletInfoOutput $(( ${SIZE_X_TRANS_VIEW} - 4 )))" "${SIZE_Y_TRANS_VIEW}" "${SIZE_X_TRANS_VIEW}"
+           --msgbox "$(makeWalletInfoOutput $(( ${SIZE_X_TRANS_VIEW} - 4 )))" 34 "${SIZE_X_TRANS_VIEW}"
 
     refreshMainMenu_DATA
 }
