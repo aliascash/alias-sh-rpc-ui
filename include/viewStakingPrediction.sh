7939e3f7a7d0a94f9e45c80460852c1af0eab04e
HLXEasy <hlxeasy@gmail.com>
2018-12-11 19:15:20 +0100

    Formated info output using separate language vars

--- include/viewStakingPrediction.sh
+++ include/viewStakingPrediction.sh
@@ -10,9 +10,47 @@
 # ============================================================================
 # Print prepared data regarding staking from RPC 'listtransactions' to stdout
 makeStakeInfoOutput() {
-    for (( _i = 0 ; _i <= ${#staking_analysis[@]} ; _i++ )) ; do
-        echo "${staking_analysis[$_i]}\n"
-    done
+    local _textWidth=70
+    echo $(fillLine "${TEXT_STAKING_ANALYSIS_TIMEFRAME}:-_-${staking_analysis[1]}" \
+                    "${_textWidth}")"\n"
+    echo $(fillLine "${TEXT_STAKING_ANALYSIS_AMOUNTS}:-_-${staking_analysis[2]}" \
+                    "${_textWidth}")"\n"
+    echo $(fillLine "${TEXT_STAKING_ANALYSIS_TOTAL_REWARD}:-_-${staking_analysis[3]}" \
+                    "${_textWidth}")"\n"
+    echo $(fillLine "${TEXT_STAKING_ANALYSIS_TOTAL_TODAY}:-_-${staking_analysis[4]}" \
+                    "${_textWidth}")"\n"
+
+    echo "\n${TEXT_ESTIMATIONS}:\n"
+    echo $(fillLine "${TEXT_STAKING_ANALYSIS_REWARD_PER_YEAR}:-_-${staking_analysis[5]}" \
+                    "${_textWidth}")"\n"
+    echo $(fillLine "${TEXT_STAKING_ANALYSIS_COINS_IN_ONE_MONTH}:-_-${staking_analysis[6]}" \
+                    "${_textWidth}")"\n"
+    echo $(fillLine "${TEXT_STAKING_ANALYSIS_STAKED_COINS_IN} ${TEXT_ONE_MONTH}:-_-${staking_analysis[7]}" \
+                    "${_textWidth}")"\n"
+    echo $(fillLine "${TEXT_STAKING_ANALYSIS_TOTAL_COINS_IN} ${TEXT_HALF_YEAR}:-_-${staking_analysis[8]}" \
+                    "${_textWidth}")"\n"
+    echo $(fillLine "${TEXT_STAKING_ANALYSIS_STAKED_COINS_IN} ${TEXT_HALF_YEAR}:-_-${staking_analysis[9]}" \
+                    "${_textWidth}")"\n"
+    echo $(fillLine "${TEXT_STAKING_ANALYSIS_TOTAL_COINS_IN} ${TEXT_ONE_YEAR}:-_-${staking_analysis[10]}" \
+                    "${_textWidth}")"\n"
+    echo $(fillLine "${TEXT_STAKING_ANALYSIS_STAKED_COINS_IN} ${TEXT_ONE_YEAR}:-_-${staking_analysis[11]}" \
+                    "${_textWidth}")"\n"
+    echo $(fillLine "${TEXT_STAKING_ANALYSIS_TOTAL_COINS_IN} ${TEXT_TWO_YEARS}:-_-${staking_analysis[12]}" \
+                    "${_textWidth}")"\n"
+    echo $(fillLine "${TEXT_STAKING_ANALYSIS_STAKED_COINS_IN} ${TEXT_TWO_YEARS}:-_-${staking_analysis[13]}" \
+                    "${_textWidth}")"\n"
+    echo $(fillLine "${TEXT_STAKING_ANALYSIS_TOTAL_COINS_IN} ${TEXT_THREE_YEARS}:-_-${staking_analysis[14]}" \
+                    "${_textWidth}")"\n"
+    echo $(fillLine "${TEXT_STAKING_ANALYSIS_STAKED_COINS_IN} ${TEXT_THREE_YEARS}:-_-${staking_analysis[15]}" \
+                    "${_textWidth}")"\n"
+    echo $(fillLine "${TEXT_STAKING_ANALYSIS_TOTAL_COINS_IN} ${TEXT_FOUR_YEARS}:-_-${staking_analysis[16]}" \
+                    "${_textWidth}")"\n"
+    echo $(fillLine "${TEXT_STAKING_ANALYSIS_STAKED_COINS_IN} ${TEXT_FOUR_YEARS}:-_-${staking_analysis[17]}" \
+                    "${_textWidth}")"\n"
+    echo $(fillLine "${TEXT_STAKING_ANALYSIS_TOTAL_COINS_IN} ${TEXT_FIVE_YEARS}:-_-${staking_analysis[18]}" \
+                    "${_textWidth}")"\n"
+    echo $(fillLine "${TEXT_STAKING_ANALYSIS_STAKED_COINS_IN} ${TEXT_FIVE_YEARS}:-_-${staking_analysis[19]}" \
+                    "${_textWidth}")"\n"
 }
 
 
@@ -23,7 +61,6 @@ makeStakeInfoOutput() {
 viewStakingPrediction() {
     unset stakes
     declare -A stakes
-    calculateLayout
     executeCURL "listtransactions" \
                 '"*",1000,0,"1"'
     getStakingPrediction
@@ -32,7 +69,7 @@ viewStakingPrediction() {
            --title "${TITLE_STAKING_INFO}" \
            --ok-label "${BUTTON_LABEL_OK}" \
            --no-shadow \
-           --msgbox "$(makeStakeInfoOutput)" 34 "${SIZE_X_TRANS_VIEW}"
+           --msgbox "$(makeStakeInfoOutput)" 26 30
 
     refreshMainMenu_DATA
 }
