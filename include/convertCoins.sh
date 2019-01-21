a182f209ace4bc0a450e6ede2b68d901db90eb03
HLXEasy <hlxeasy@gmail.com>
2019-01-21 20:37:48 +0100

    Added first version of coin conversion function

---
+++ include/convertCoins.sh
@@ -0,0 +1,148 @@
+#!/bin/bash
+# ============================================================================
+#
+# This is a component of the Spectrecoin shell rpc ui
+#
+# Author: 2019 HLXEasy
+#
+# ============================================================================
+
+# ============================================================================
+# Ask user in which direction he wants to convert coins.
+# Return:
+# ${CONVERT_NOTHING} .. Nothing should be converted, back to main manu
+# ${CONVERT_ANON_TO_PUBLIC} .. Convert anon coins to public coins
+# ${CONVERT_PUBLIC_TO_ANON} .. Convert public coins to anon coins
+getConversionDestination() {
+    calculateLayout
+    dialog --no-shadow \
+        --begin 0 0 \
+        --no-lines \
+        --infobox "" "${currentTPutLines}" "${currentTPutCols}" \
+        \
+        --and-widget \
+        --colors \
+        --title "${TITLE_PLEASE_CHOOSE}" \
+        --ok-label "${BUTTON_LABEL_PUBLIC_TO_ANON}" \
+        --cancel-label "${BUTTON_LABEL_ANON_TO_PUBLIC}" \
+        --extra-button --extra-label "${BUTTON_LABEL_MAIN_MENU}" \
+        --default-button "extra" \
+        --yesno "${TEXT_CONVERSION_QUESTION}" 7 60
+    exit_status=$?
+    case ${exit_status} in
+        ${DIALOG_EXTRA})
+            return ${CONVERT_NOTHING};;
+        ${DIALOG_CANCEL})
+            return ${CONVERT_ANON_TO_PUBLIC};;
+        ${DIALOG_OK})
+            return ${CONVERT_PUBLIC_TO_ANON};;
+    esac
+}
+
+# ============================================================================
+# Ask user for coin conversion direction, amount and narration
+# and convert the given amount.
+convertCoins() {
+    local _destinationAddress=''
+    local _buffer=''
+    local _narration=''
+    local _balance=0
+    local _convertDialogTitle=''
+    local _headline=''
+    local _conversionCmd=''
+    local _ringSizeParam=''
+
+    getConversionDestination
+    case $? in
+        ${CONVERT_NOTHING})
+            return
+            ;;
+        ${CONVERT_PUBLIC_TO_ANON})
+            executeCURL "liststealthaddresses"
+            curl_result_global=${curl_result_global//','/'\n'}
+            curl_result_global=${curl_result_global//'['/''}
+            _destinationAddress=$(echo ${curl_result_global} | sed -e 's/.*Stealth Address://g' -e 's/ -.*//g')
+            _headline="${TEXT_BALANCE}: ${_balance} ${TEXT_CURRENCY}"
+            _convertDialogTitle="${TITLE_CONVERT}: ${TEXT_CURRENCY} > ${TEXT_CURRENCY_ANON}"
+            _conversionCmd="sendspectoanon"
+            _balance=$(echo "scale=8 ; ${info_global[${WALLET_BALANCE}]}+${info_global[${WALLET_STAKE}]}" | bc)
+            if [[ ${_balance} == '.'* ]]; then
+                _balance="0"${_balance}
+            fi
+            ;;
+        ${CONVERT_ANON_TO_PUBLIC})
+            executeCURL "getaddressesbyaccount" "\"Default Address\""
+            curl_result_global=${curl_result_global//','/'\n'}
+            curl_result_global=${curl_result_global//'['/''}
+            _destinationAddress=${curl_result_global//']'/''}
+            _headline="${TEXT_BALANCE}: ${_balance} ${TEXT_CURRENCY_ANON}"
+            _convertDialogTitle="${TITLE_CONVERT}: ${TEXT_CURRENCY_ANON} > ${TEXT_CURRENCY}"
+            _conversionCmd="sendanontospec"
+            _balance=$(echo "scale=8 ; ${info_global[${WALLET_BALANCE_ANON}]}+${info_global[${WALLET_STAKE_ANON}]}" | bc)
+            if [[ ${_balance} == '.'* ]]; then
+                _balance="0"${_balance}
+            fi
+            _ringSizeParam=',10'
+            ;;
+    esac
+
+    exec 3>&1
+    oldIFS=${IFS}
+    IFS="|"
+    _buffer=$(dialog --backtitle "${TITLE_BACK}" \
+        --ok-label "${BUTTON_LABEL_SEND}" \
+        --cancel-label "${BUTTON_LABEL_MAIN_MENU}" \
+        --no-shadow \
+        --colors \
+        --title "${_convertDialogTitle}" \
+        --form "${_headline}" 12 65 0 \
+        "${TEXT_AMOUNT_TO_CONVERT}:" 2 1 "${_amount}" 2 22 20 0 \
+        "${TEXT_SEND_NARRATION}:" 4 1 "${_narration}" 4 22 24 0 \
+        " " 5 1 "" 5 0 0 0\
+        2>&1 1>&3)
+    exit_status=$?
+    IFS=${oldIFS}
+    exec 3>&-
+    case ${exit_status} in
+        ${DIALOG_CANCEL})
+            refreshMainMenu_GUI;;
+        ${DIALOG_ESC})
+            refreshMainMenu_GUI;;
+        ${DIALOG_OK})
+            # Convert buffer into array
+            # $sendInput[0] = Amount
+            # $sendInput[1] = Narration
+            mapfile -t sendInput <<< "${_buffer}"
+            if [[ ${sendInput[0]} =~ ^[0-9]{0,8}[.]{0,1}[0-9]{0,8}$ ]] && [[ 1 -eq "$(echo "${sendInput[0]} > 0" | bc)" ]]; then
+                if [[ "${info_global[${WALLET_UNLOCKED_UNTIL}]}" == "${TEXT_WALLET_IS_UNLOCKED}" ]]; then
+                    # If wallet is unlocked, we have to look it first
+                    executeCURL "walletlock"
+                fi
+                if [[ "${info_global[${WALLET_UNLOCKED_UNTIL}]}" != "${TEXT_WALLET_HAS_NO_PW}" ]]; then
+                    passwordDialog "60" "false"
+                fi
+                if [[ -z "${sendInput[1]}" ]] ; then
+                    # No narration given
+                    executeCURL "${_conversionCmd}" "\"${_destinationAddress}\",${sendInput[0]}${_ringSizeParam}"
+                else
+                    executeCURL "${_conversionCmd}" "\"${_destinationAddress}\",${sendInput[0]}${_ringSizeParam},\"${sendInput[1]}\""
+                fi
+                if [[ "${info_global[${WALLET_UNLOCKED_UNTIL}]}" != "${TEXT_WALLET_HAS_NO_PW}" ]]; then
+                    executeCURL "walletlock"
+                fi
+                if [[ "${info_global[${WALLET_UNLOCKED_UNTIL}]}" == "${TEXT_WALLET_IS_UNLOCKED}" ]]; then
+                    simpleMsg "" \
+                              "${TEXT_SEND_UNLOCK_WALLET_AGAIN}" \
+                              "${BUTTON_LABEL_I_HAVE_UNDERSTOOD}"
+                    unlockWalletForStaking
+                fi
+                refreshMainMenu_GUI
+            else
+                errorHandling "${ERROR_SEND_INVALID_AMOUNT}"
+                convertCoins
+            fi
+            ;;
+    esac
+    errorHandling "${ERROR_CONVERT_FATAL}" \
+                  1
+}
