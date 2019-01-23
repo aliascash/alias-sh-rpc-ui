cf548c3a041c0c8c551dc12f62ead695a0122d32
HLXEasy <hlxeasy@gmail.com>
2019-01-23 18:39:54 +0100

    Implemented update of binaries

---
+++ include/updateBinaries.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# ============================================================================
+#
+# This is a component of the Spectrecoin shell rpc ui
+#
+# Author: 2019 HLXEasy
+#
+# ============================================================================
+
+
+# ============================================================================
+# Goal:
+updateBinaries() {
+    dialog --backtitle "${TITLE_BACK}" \
+        --title "${TITLE_UPDATE_BINARIES}" \
+        --yesno "${TEXT_QUESTION_DO_UPDATE}" 7 40
+
+    # Get exit status
+    # 0 means user hit [yes] button.
+    # 1 means user hit [no] button.
+    # 255 means user hit [Esc] key.
+    exit_status=$?
+    case ${exit_status} in
+        0)
+            reset
+            info "Stopping spectrecoind"
+            sudo systemctl stop spectrecoind
+            echo ''
+            info "Downloading and starting update script"
+            bash <(curl -s https://raw.githubusercontent.com/spectrecoin/installer/simpleUpdater/linux/updateSpectrecoin.sh)
+            info "Update finished, press return to restart spectrecoind"
+            read a
+            ;;
+        *)
+            dialog --backtitle "${TITLE_BACK}" \
+                --title "${TITLE_UPDATE_BINARIES}" \
+                --msgbox "${TEXT_UPDATE_CANCELED}" 6 20
+            ;;
+    esac
+    refreshMainMenu_DATA
+}
