c22d91a4399d74748a7755bd801943bbdac3516d
HLXEasy <hlxeasy@gmail.com>
2019-04-01 00:13:29 +0200

    Implemented new helper function to open a dialog tailbox

---
+++ include/viewLog.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# ============================================================================
+#
+# This is a component of the Spectrecoin shell rpc ui
+#
+# Author: 2019-03 HLXEasy
+#
+# ============================================================================
+
+# ============================================================================
+# Open a dialog tailbox and show debug.log
+viewLog() {
+    dialog --backtitle "${TITLE_BACK}" \
+           --no-shadow \
+           --colors \
+           --begin 2 2 \
+           --no-lines \
+           --infobox "${TEXT_LOGFILE_HEADER}" ${LOG_TAIL_WINDOW_HEADER} ${LOG_TAIL_WINDOW_Y} \
+           \
+            --and-widget \
+           --title " ${logfile} " \
+           --no-shadow \
+           --colors \
+           --ok-label "${BUTTON_LABEL_CLOSE}" \
+           --tailbox "${logfile}" \
+           ${LOG_TAIL_WINDOW_X} ${LOG_TAIL_WINDOW_Y}
+}
