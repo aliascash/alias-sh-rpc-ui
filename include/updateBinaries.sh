3be2e7d9be16330fec83448d485bafb8772b8e00
HLXEasy <hlxeasy@gmail.com>
2019-04-04 23:53:03 +0200

    First steps towards update dialog to choose version to install

--- include/updateBinaries.sh
+++ include/updateBinaries.sh
@@ -8,12 +8,28 @@
 # ============================================================================
 
 
+chooseVersionToInstall() {
+    dialog \
+        --backtitle "${TITLE_BACK}" \
+        --colors \
+        --no-shadow \
+        --title title \
+        --radiolist Text 23 50 10 $(for i in $(curl -L -s https://api.github.com/repos/spectrecoin/spectre/releases | grep -e tag_name -e published_at -e tarball_url | cut -d: -f2 | cut -d '"' -f2) ; do echo "${i:0:10}" ; done ) 2>/tmp/choice.txt
+}
+
 # ============================================================================
 # Goal:
 updateBinaries() {
-    dialog --backtitle "${TITLE_BACK}" \
+    dialog \
+        --backtitle "${TITLE_BACK}" \
+        --colors \
+        --no-shadow \
         --title "${TITLE_UPDATE_BINARIES}" \
-        --yesno "\n${TEXT_QUESTION_DO_UPDATE}" 7 40
+        --ok-label "${BUTTON_LABEL_UPDATE_TO_LATEST_RELEASE}" \
+        --cancel-label "${BUTTON_LABEL_UPDATE_CHOOSE_VERSION}" \
+        --extra-button --extra-label "${BUTTON_LABEL_MAIN_MENU}" \
+        --default-button "extra" \
+        --yesno "${TEXT_QUESTION_DO_UPDATE}" 9 40
 
     # Get exit status
     # 0 means user hit [yes] button.
@@ -21,7 +37,16 @@ updateBinaries() {
     # 255 means user hit [Esc] key.
     exit_status=$?
     case ${exit_status} in
-        0)
+        ${DIALOG_EXTRA})
+            dialog \
+                --backtitle "${TITLE_BACK}" \
+                --title "${TITLE_UPDATE_BINARIES}" \
+                --msgbox "\n${TEXT_UPDATE_CANCELED}" 9 40
+            ;;
+        ${DIALOG_CANCEL})
+            chooseVersionToInstall
+            ;;
+        ${DIALOG_OK})
             reset
             info "Stopping spectrecoind"
             sudo systemctl stop spectrecoind
@@ -30,11 +55,6 @@ updateBinaries() {
             curl -L -s https://raw.githubusercontent.com/spectrecoin/installer/master/linux/updateSpectrecoin.sh | sudo bash -s
             info "Update finished, press return to restart spectrecoind"
             read a
-            ;;
-        *)
-            dialog --backtitle "${TITLE_BACK}" \
-                --title "${TITLE_UPDATE_BINARIES}" \
-                --msgbox "\n${TEXT_UPDATE_CANCELED}" 6 20
             ;;
     esac
     refreshMainMenu_DATA
