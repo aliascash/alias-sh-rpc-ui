7f929b64b9bd187f392fa627d04af3f56982eb43
HLXEasy <hlxeasy@gmail.com>
2021-02-26 22:34:41 +0100

    Improved language handling:
    - Skip duplicated load if configured language is default language
    - Show message box if configured language not found

--- include/handleSettings.sh
+++ include/handleSettings.sh
@@ -16,18 +16,31 @@ settingsfile=~/.aliaswallet-ui-settings
 # ============================================================================
 # Goal:
 handleSettings() {
+    local defaultLanguage="en"
     # Load default language, which define and fill all variables.
-    loadLanguage en
+    loadLanguage ${defaultLanguage}
     if [[ -e "${settingsfile}" ]] ; then
         # Settingsfile found
         . "${settingsfile}"
         if [[ -n "${UI_LANGUAGE}" ]] ; then
             # Language var not empty
-            if [[ -e "locale/ui_content_${UI_LANGUAGE}.yml" ]] ; then
+            if [[ "${UI_LANGUAGE}" = "${defaultLanguage}" ]] ; then
+                # Configured language is default language, skip loading it again
+                :
+            elif [[ -e "locale/ui_content_${UI_LANGUAGE}.yml" ]] ; then
                 # Language file exists, so load it "on top" of already loaded
                 # default language. Not existing entries on the choosen
                 # language stay at their default value.
                 loadLanguage "${UI_LANGUAGE}"
+            else
+                # Configured language not found
+                # shellcheck disable=SC2059
+                local message=$(printf "${ERROR_LANGUAGE_NOT_FOUND}" "${UI_LANGUAGE}" "${defaultLanguage}")
+                dialog --title " ${TITLE_ERROR} " \
+                    --msgbox "\n$message" \
+                    10 38
+                UI_LANGUAGE="${defaultLanguage}"
+                updateSettings
             fi
         fi
     fi
